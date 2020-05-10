df284d9eabeee842a123a986415e9c56e0215e87
David Ross <dross78375@gmail.com>
2020-05-10 18:18:12 +0100

    Rename files to something more logical

---
+++ query/joining-complex.sql
@@ -0,0 +1,21 @@
+-- MUTLI TABLE JOIN
+
+SELECT
+  city.name AS city,
+  country.name AS country,
+  concat(currency.code, ' (', currency.symbol, ')') AS currency
+FROM
+  country
+INNER JOIN city ON country.id = city.country
+INNER JOIN currency ON country.currency = currency.id;
+
+/*
+   city    |         country          | currency
+-----------+--------------------------+-----------
+ london    | united kingdom           | gbp (£)
+ edinburgh | united kingdom           | gbp (£)
+ new york  | united states of america | usd ($)
+ san diego | united states of america | usd ($)
+ geneva    | switzerland              | chf (fr.)
+(5 rows)
+*/
